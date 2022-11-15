1c9a47564efc449bdda7f86449921b87fced15df
mnomanme <abdullah.noman083@gmail.com>
2022-11-16 00:32:48 +0600

    solved but design is not fancy :rocket:

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,22 @@
-/* eslint-disable react/prop-types */
-import React from 'react';
+const Product = ({ category, description, image, price, title, rating }) => {
+	const { count, rate } = rating;
 
-const Product = () => {
-  return (
-      <article className="product">
-      <img src="" alt="" />
-      <div className="product__details">
-        <h4 className="product__title">product title</h4>
-        <p>Price: $ product price</p>
-        <p>Rating: product rating rate/5</p>
-        <p className="product__desc">Description: product.description</p>
-        <button className="product__btn btn">Add to cart</button>
-      </div>
-    </article>
-  );
+	return (
+		<>
+			<article className="product">
+				<img className="product__img" src={image} alt={title} />
+
+				<div className="product__details">
+					<h4 className="product__title">{title}</h4>
+					<p className="product__price">Price: ${price}</p>
+					<p className="product__rating">Rating: {rate}/5</p>
+					<p className="product__desc"> {description}</p>
+					<p>{category}</p>
+					<button className="product__btn btn">Add to cart</button>
+				</div>
+			</article>
+		</>
+	);
 };
 
 export default Product;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,1 +1,14 @@
-/* eslint-disable react/prop-types */
+import Product from './Product';
+
+export const Products = ({ products }) => {
+	console.table(products);
+	return (
+		<>
+			<section className="products">
+				{products.map((product) => {
+					return <Product key={product.id} {...product} />;
+				})}
+			</section>
+		</>
+	);
+};
